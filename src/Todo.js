d1456870775906abb8ba55ec494c06b44d2e9146
Samlogy <senanisammy@gmail.com>
2021-02-13 10:39:19 +0100

    infinite scroll

--- src/Todo.js
+++ src/Todo.js
@@ -6,46 +6,81 @@ import Modal from './Modal';
 import './style.css';
 
 const Todo = () => {
-    const [todo, setTodo] = useState({ userId: 1, id: 1, title: '', completed: false });
-    const [todos, setTodos] = useState({ data: [], loading: false });
-    const [label, setLabel] = useState('all');
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [todo, setTodo] = useState({ userId: 1, id: 1, title: '', completed: false }); // single todo
+    const [todos, setTodos] = useState({ data: [], loading: false }); // all todos
+    const [label, setLabel] = useState('all'); // label for Cpt
+    const [isModalOpen, setIsModalOpen] = useState(false); // modal box
+    const [searchOp, setSearchOp] = useState({ text: '', result: [] }); // search Feature
 
     // close modal (set isModalOpen, true)
     const closeModal = () => setIsModalOpen(false);
     const openModal = () => setIsModalOpen(true);
 
-    useEffect(() => {  getAllTodos() }, []);
-    
+    // exclude column list from filter "Search"
+    const excludeColumns = ['title'];
+
+    useEffect(() => {  getAllTodos(10) }, []); // set limit (nbr max of items to render)
+
+    const clearForm = () => setTodo({ userId: '', id: '', title: '', completed: '' });
+
+    const AddFeature = label => {
+        clearForm(); setLabel(label);
+    };
+
     // API calls
     const add = e => {
         e.preventDefault();
+
         const url = '';
         fetch(url, { method: 'POST', body: JSON.stringify(todo)})
         .then(res => res.json())
-        .then(json => setTodos({ data: json, loading: false }))
-        .catch(error => console.log(error.message));
+        .then(res => setTodos({ data: res, loading: false }))
+        .catch(err => console.log(err.mesage));
     };
     const remove = todo => {
         const url = `/postId=${todo.id}`;
+
         fetch(url, { method: 'DELETE' })
         .then(res => res.json())
-        .catch(error => console.log(error.message));
+        .then(res => setTodos({ data: res, loading: false }))
+        .catch(err => console.log(err.mesage));
+
         closeModal();
     };
-    const edit = todo => {
+    const edit = (e, todo) => {
+        e.preventDefault();
+
         const url = `/postId=${todo.id}`;
         fetch(url, { method: 'PUT', body: JSON.stringify(todo) })
         .then(res => res.json())
-        .catch(error => console.log(error.message));
+        .then(res => setTodos({ data: res, loading: false }))
+        .catch(err => console.log(err.mesage));
     };
-    const getAllTodos = () => {
+    const getAllTodos = limit => {
         setTodos({...todos, loading: true })
-        fetch('https://jsonplaceholder.typicode.com/todos')
+        const url ='https://jsonplaceholder.typicode.com/todos';
+        fetch(url)
         .then(res => res.json())
-        .then(json => setTodos({ data: json, loading: false }))
-        .catch(error => console.log(error.message));
+        .then(res => setTodos({ data: res, loading: false }))
+        .catch(err => console.log(err.mesage));
     };
+
+    // Search Feature
+    const handleSearch = value => {
+        setSearchOp({...searchOp, text: value });
+        filterData(value);
+    };
+    const filterData = value => {
+        const lowercasedValue = value.toLowerCase().trim();
+        // if (lowercasedValue === "") return 'todos.data';
+        // if (lowercasedValue !== "") {
+        //         const filteredData = todos.data.filter(item => 
+        //             item.toString().toLowerCase().includes(lowercasedValue) )
+        //     setSearchOp({...searchOp, result: filteredData });
+        // }
+    };
+    // return Object.keys(item).some(key =>
+    //     excludeColumns.includes(key) ? false : item[key].toString().toLowerCase().includes(lowercasedValue)
 
     // components list
     const addTodo = () => {
@@ -53,14 +88,35 @@ const Todo = () => {
                     <h2>Add Todo</h2>
     
                     <form method="post" onSubmit={e => add(e)}> 
-                        <input type="number" name="id" placeholder="ID" 
-                            onChange={e => setTodo({...todo, id: e.target.value})} value={todo.id} min="0" />
-    
-                        <input type="text" name="title" placeholder="Title" 
-                            onChange={e => setTodo({...todo, title: e.target.value})} value={todo.title}/>
-    
-                        {/* <textarea name="text" placeholder="Text ..." 
-                            onChange={e => setTodo({...todo, text: e.target.value})} value={todo.text}></textarea> */}
+                        <div className='form-input'>
+                            <label htmlFor='id'> ID </label>
+                            <input type="number" name="id" placeholder="ID" id='id'
+                                onChange={e => setTodo({...todo, id: e.target.value})} value={todo.id} min="0" />
+                        </div>
+
+                        <div className='form-input'>
+                            <label htmlFor='title'> Title  </label>
+                            <input type="text" name="title" placeholder="Title" id='title'
+                                onChange={e => setTodo({...todo, title: e.target.value})} value={todo.title}/>
+                        </div>
+                        
+                        <div className='form-input'>
+                            <label> Completed </label>
+
+                            <div className='completed'>
+                                <div>
+                                    <label> Yes </label> 
+                                    <input type="checkbox" name="completed" id='completed_yes'
+                                        onChange={e => setTodo({...todo, completed: e.target.value})} value={todo.completed} />
+                                </div>
+
+                                <div>
+                                    <label> No </label>    
+                                    <input type="checkbox" name="completed" id='completed_no'
+                                        onChange={e => setTodo({...todo, completed: e.target.value})} value={todo.completed} />
+                                </div>
+                            </div>
+                        </div>
     
                         <button type="submit"> ADD TASK </button>
                     </form>
@@ -71,11 +127,35 @@ const Todo = () => {
                     <h2> Edit Todo </h2>
     
                     <form method="post" onSubmit={edit}> 
-                        <input type="number" name="id" placeholder="ID" 
-                            onChange={e => setTodo({...todo, id: e.target.value})} value={todo.id} min="0" />
-    
-                         <textarea name="text" name="title" placeholder="Title" 
-                            onChange={e => setTodo({...todo, title: e.target.value})} value={todo.title}></textarea>
+                        <div className='form-input'>
+                            <label htmlFor='id'> ID </label>
+                            <input type="number" name="id" placeholder="ID" id='id'
+                                onChange={e => setTodo({...todo, id: e.target.value})} value={todo.id} min="0" />
+                        </div>
+
+                        <div className='form-input'>
+                            <label htmlFor='title'> Title  </label>
+                            <input type="text" name="title" placeholder="Title" id='title'
+                                onChange={e => setTodo({...todo, title: e.target.value})} value={todo.title}/>
+                        </div>
+                        
+                        <div className='form-input'>
+                            <label> Completed </label>
+
+                            <div className='completed'>
+                                <div>
+                                    <label> Yes </label> 
+                                    <input type="checkbox" name="completed" id='completed_yes'
+                                        onChange={e => setTodo({...todo, completed: e.target.value})} value={todo.completed} />
+                                </div>
+
+                                <div>
+                                    <label> No </label>    
+                                    <input type="checkbox" name="completed" id='completed_no' checked={todo.completed}
+                                        onChange={e => setTodo({...todo, completed: e.target.value})} value={todo.completed} />
+                                </div>
+                            </div>
+                        </div>
     
                         <button type="submit"> EDIT TASK </button>
                     </form>
@@ -109,6 +189,10 @@ const Todo = () => {
     const todoList = () => {
         return  <div className='todo-list'>
                     <h2> Todo List</h2>
+                    
+                    { searchTodo() }
+                    { (Array.isArray(searchOp.result) && searchOp.result.length > 0) && <span>Suggestions: {searchOp.result.length} </span> }
+
                     <ul>
                         {!todos.loading && 
                             <div className='item-labels'>
@@ -130,9 +214,46 @@ const Todo = () => {
                                     <BsPencilSquare onClick={() => {setTodo(todo);setLabel('edit')}} className='icon' />
                                     <BsTrashFill onClick={() => { setTodo(todo);openModal();}} className='icon' />
                                 </div>
-                            </div>  ) : 'Loading...' }
+                            </div>  ) :  Spinner() }
                     </ul>
                 </div>
+    };
+    const searchTodo = () => {
+        return  <div className='search-todo'>
+                    <span> Search: </span> 
+                    <input type="text" placeholder="Type to search..." value={searchOp.text} 
+                            onChange={e => handleSearch(e.target.value)} />
+                </div>
+    };
+    const Spinner = () => {
+        return  <div className="loading">
+                    <div className="ball"></div>
+                    <div className="ball"></div>
+                    <div className="ball"></div>
+                </div>
+    };
+
+    // Infinite Scroll
+    window.addEventListener('scroll', () => {
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+        // console.log( { scrollTop, scrollHeight, clientHeight });
+        if(clientHeight + scrollTop >= scrollHeight - 5) beforeFetching(); // show the loading animation
+    });
+
+    const beforeFetching = () => {
+        console.log('Before Fetch !')
+        setTodos({...todos, loading: true }); // show spinner before loading data
+        setTimeout(moreTodos, 1000); // load more data
+        console.log('more Todos !')
+    };
+    
+    const moreTodos = async () => {
+        // show spinner, load data & set it in state
+        const url ='https://jsonplaceholder.typicode.com/todos';
+        fetch(url)
+        .then(res => res.json())
+        .then(res => {setTodos({ data: res, loading: false }); console.log('Fetching !')})
+        .catch(err => console.log(err.mesage));
     };
 
     // Display Component according to user's choices
@@ -143,19 +264,17 @@ const Todo = () => {
             case 'edit': return editTodo(todo);
         }
     };
-
-    
-
     
     return (
         <div className="todo-container">
             <ul className='menu'>
                 <li onClick={() => setLabel('all')}> All Tasks </li>
-                <li onClick={() => setLabel('add')}> Add Task </li>
+                <li onClick={() => { AddFeature('add') }}> Add Task </li>
             </ul>
 
             <div className='container'>
-                { label && label ? showCpt(label) : <p> Loading... </p> }
+                { label && label ? showCpt(label) :  'loading label' }
+                {/* { todos.loading ? 'loading ' : '' } */}
                 { isModalOpen && removeTodo() }
             </div>
         </div>
